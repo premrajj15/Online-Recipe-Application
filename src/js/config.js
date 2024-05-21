179fd896bdeffe01eee21e956097b09b6859afd9
premraj <premrajpadakanti@gmail.com>
2024-05-21 19:02:09 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ae2fd4de-8a08-433d-843e-8e0b82309bef';
+export const MODAL_CLOSE_SEC = 2.5;
